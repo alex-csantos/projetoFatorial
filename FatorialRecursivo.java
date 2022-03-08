425fdb5cf8b10e9840c8e1b347d44c1c07669276
Alex Santos <alex.csantos02@hotmail.com>
2022-03-08 01:09:08 -0300

    fatorial recursivo implementado

---
+++ FatorialRecursivo.java
@@ -0,0 +1,13 @@
+public class FaorialRecursivo{
+    public static int calcularFatorialRecursivo(int n) throw illegalArgumentException{
+        if(n==0)
+            return 1;
+
+        return n * calcularFatorialRecursivo(n-1);
+    }
+
+    public static void main(String[] args){
+        System.out.println(calcularFatorialRecursivo(5))
+
+    }
+}
